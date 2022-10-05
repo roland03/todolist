5e7431af62fa58690485c53cb105ebddd28acf86
Roland Valentino <rolandvalentino111@gmail.com>
2022-10-05 08:33:55 +0300

    Changed the port

--- app.js
+++ app.js
@@ -116,8 +116,11 @@ if(listName==="Today"){
 app.get("/about", function(req, res){
   res.render("about");
 })
+let port = process.env.PORT;
+if (port == null || port == "") {
+  port = 3000;
+}
 
-
-app.listen(3000, function(){
-  console.log("Todo-list server is running on port 3000 .");
+app.listen(port, function(){
+  console.log("Todo-list server is running .");
 });
